a4401a3ea684ebcb5e1b0105292a800244bc6349
Juan Bobadilla A <86940534+KiraxPlay@users.noreply.github.com>
2026-01-12 22:03:27 -0500

    Update urls.py

--- bd_Smartgalpon_api/bd_Smartgalpon/urls.py
+++ bd_Smartgalpon_api/bd_Smartgalpon/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-    path('api/ponedoras/', include('api.urlsPonedoras')),
+    path('api/', include('api.urls' , namespace='engorde')),
+    path('api/ponedoras/', include('api.urlsPonedoras', namespace='ponedoras')),
 ]
