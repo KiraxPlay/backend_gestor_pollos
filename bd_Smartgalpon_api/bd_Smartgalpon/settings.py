15adb5ba9248698164ca47f7e59b8cc30da6e309
sempiternalco2@hotmail.com <sempiternalco2@hotmail.com>
2026-03-26 22:47:23 -0500

    mas correcciones para render

--- bd_Smartgalpon_api/bd_Smartgalpon/settings.py
+++ bd_Smartgalpon_api/bd_Smartgalpon/settings.py
@@ -69,11 +69,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
     'rest_framework',
     'corsheaders',
-
     'api',
+    'reportes'
 ]
 
 # -----------------------------------------------------------------------------
